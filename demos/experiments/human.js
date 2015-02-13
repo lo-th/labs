891e95cdd1b0efe6e3d4e3c0fc9764b250371521
lo-th <3dflashlo@gmail.com>
2015-02-13 12:58:01 +0100

    up

--- demos/experiments/human.js
+++ demos/experiments/human.js
@@ -37,41 +37,27 @@ function onload(){
     body.scale.set(size,size,size);
     suit.scale.set(size,size,size);
 
-    var tx0 = THREE.ImageUtils.loadTexture( 'images/dianna/full.jpg');
-    var tx1 = THREE.ImageUtils.loadTexture( 'images/dianna/head.jpg');
-    var tx2 = THREE.ImageUtils.loadTexture( 'images/dianna/hair.png');
-    var tx3 = THREE.ImageUtils.loadTexture( 'images/dianna/eye_cont.png');
-    var tx4 = THREE.ImageUtils.loadTexture( 'images/dianna/teethLow.png');
-    var tx5 = THREE.ImageUtils.loadTexture( 'images/dianna/teethUp.png');
-    var tx6 = THREE.ImageUtils.loadTexture( 'images/dianna/sock.jpg');
-    var tx7 = THREE.ImageUtils.loadTexture( 'images/dianna/tongue.jpg');
-    var tx8 = THREE.ImageUtils.loadTexture( 'images/dianna/teethLow_n.jpg');
-    var tx9 = THREE.ImageUtils.loadTexture( 'images/dianna/teethUp_n.jpg');
-    var tx10 = THREE.ImageUtils.loadTexture( 'images/dianna/head_n.jpg');
-    var tx11 = THREE.ImageUtils.loadTexture( 'images/dianna/body_n.jpg');
+    var texNames = [
+        'full.jpg','head.jpg','hair.png','eye_cont.png',
+        'teethLow.png','teethUp.png','sock.jpg','tongue.jpg',
+        'teethLow_n.jpg','teethUp_n.jpg','head_n.jpg','body_n.jpg'
+    ];
+    var textures = [];
+    var i = texNames.length;
+    while(i--){
+        textures[i] = THREE.ImageUtils.loadTexture( 'images/dianna/'+texNames[i]);
+        textures[i].flipY = false;
+    }
 
-    tx0.flipY = false;
-    tx1.flipY = false;
-    tx2.flipY = false;
-    tx3.flipY = false;
-    tx4.flipY = false;
-    tx5.flipY = false;
-    tx6.flipY = false;
-    tx7.flipY = false;
-    tx8.flipY = false;
-    tx9.flipY = false;
-    tx10.flipY = false;
-    tx11.flipY = false;
-
-    matHead = new V.Shader('Spherical', {map:tx1, normalMap:tx10, skinning:true, morphTargets:true, env:envbase, useMap:1, useNormal:1, reflection:0.2});
-    matBody = new V.Shader('Spherical', {map:tx0, normalMap:tx11, skinning:true, morphTargets:false, env:envbase, useMap:1, useNormal:1, reflection:0.2});
-    matSuit = new V.Shader('Spherical', {map:tx0, normalMap:tx11, skinning:true, morphTargets:false, env:envbase, useMap:1, useNormal:1, reflection:0.8});
-    matCils = new V.Shader('Spherical', {map:tx2, morphTargets:true, env:envbase, useMap:1, reflection:0.5, transparent:true});
-    matEyeL_lo = new V.Shader('Spherical', {map:tx3, morphTargets:true, env:envbase, useMap:1, reflection:0.5, transparent:true});
-    matTeethLower = new V.Shader('Spherical', {map:tx4, normalMap:tx8, morphTargets:true, env:envbase, useMap:1, useNormal:1, reflection:0.5, transparent:true});
-    matTeethUpper = new V.Shader('Spherical', {map:tx5, normalMap:tx9, env:envbase, useMap:1, useNormal:1, reflection:0.5, transparent:true});
-    matSock = new V.Shader('Spherical', {map:tx6, morphTargets:true, env:envbase, useMap:1, reflection:0.5});
-    matTongue = new V.Shader('Spherical', {map:tx7, morphTargets:true, env:envbase, useMap:1, reflection:0.5});
+    matHead = new V.Shader('Spherical', {map:textures[1], normalMap:textures[10], skinning:true, morphTargets:true, env:envbase, useMap:1, useNormal:1, reflection:0.2});
+    matBody = new V.Shader('Spherical', {map:textures[0], normalMap:textures[11], skinning:true, morphTargets:false, env:envbase, useMap:1, useNormal:1, reflection:0.2});
+    matSuit = new V.Shader('Spherical', {map:textures[0], normalMap:textures[11], skinning:true, morphTargets:false, env:envbase, useMap:1, useNormal:1, reflection:0.8});
+    matCils = new V.Shader('Spherical', {map:textures[2], morphTargets:true, env:envbase, useMap:1, reflection:0.5, transparent:true, side:THREE.DoubleSide});
+    matEyeL_lo = new V.Shader('Spherical', {map:textures[3], morphTargets:true, env:envbase, useMap:1, reflection:0.5, transparent:true});
+    matTeethLower = new V.Shader('Spherical', {map:textures[4], normalMap:textures[8], morphTargets:true, env:envbase, useMap:1, useNormal:1, reflection:0.5, transparent:true});
+    matTeethUpper = new V.Shader('Spherical', {map:textures[5], normalMap:textures[9], env:envbase, useMap:1, useNormal:1, reflection:0.5, transparent:true});
+    matSock = new V.Shader('Spherical', {map:textures[6], morphTargets:true, env:envbase, useMap:1, reflection:0.5});
+    matTongue = new V.Shader('Spherical', {map:textures[7], morphTargets:true, env:envbase, useMap:1, reflection:0.5});
 
     env.add(matHead);
     env.add(matBody);
@@ -87,40 +73,26 @@ function onload(){
     v.pool.meshes.dianna.sock.material = matSock;
     v.pool.meshes.dianna.tongue.material = matTongue;
 
-   /* for(var j=0; j<body.skeleton.bones.length; j++){
-        var bone = body.skeleton.bones[j]
-        var name = bone.name;
-        bodyBones[name] = bone;
-        //if(name='Bone006')bone.add(head);
-    }*/
-
     for(var j=0; j<head.skeleton.bones.length; j++){
         var bone = head.skeleton.bones[j]
         var name = bone.name;
         headBones[name] = bone;
-        //bone.matrix.copy(bodyBones[name].matrix)
-        //bone.matrixWorld.copy(bodyBones[name].matrixWorld)
-        //if(name='Bone006')bone.matrixWorld = bodyBones[name].matrixWorld.clone() ;
-        //bone.matrixAutoUpdate = false;
-       // bone.matrixWorldNeedsUpdate = true;
         console.log(name)
     }
 
+    var texEyes = [
+        'eye.jpg','eye_n.png','refl.jpg'
+    ];
+    var texturesEyes = [];
+    var i = texEyes.length;
+    while(i--){
+        texturesEyes[i] = THREE.ImageUtils.loadTexture( 'images/dianna/'+texEyes[i]);
+        texturesEyes[i].format = THREE.RGBFormat;
+        texturesEyes[i].wrapS = texturesEyes[i].wrapT = THREE.RepeatWrapping;
+        texturesEyes[i].minFilter = texturesEyes[i].magFilter = THREE.LinearFilter;
+    }
 
-    var ex0 = THREE.ImageUtils.loadTexture( 'images/dianna/eye.jpg');
-    var ex1 = THREE.ImageUtils.loadTexture( 'images/dianna/eye_n.png');
-    var ex2 = THREE.ImageUtils.loadTexture( 'images/dianna/refl.jpg');
-
-
-    ex0.format = THREE.RGBFormat;
-    ex0.wrapS = ex0.wrapT = THREE.RepeatWrapping;
-    ex0.minFilter = ex0.magFilter = THREE.LinearFilter;
-    ex1.format = THREE.RGBFormat;
-    ex1.wrapS = ex1.wrapT = THREE.RepeatWrapping;
-    ex1.minFilter = ex1.magFilter = THREE.LinearFilter;
-
-
-    matEye = new V.Shader('Eye', {texEyeCol:ex0, texEyeNrm:ex1, env:envbase, texEnvRfl:ex2});
+    matEye = new V.Shader('Eye', {texEyeCol:texturesEyes[0], texEyeNrm:texturesEyes[1], env:envbase, texEnvRfl:texturesEyes[2]});
     env.add(matEye);
     var ball = new THREE.Mesh( new THREE.SphereGeometry( 0.53, 32,16 ), matEye );
     var ball2 = new THREE.Mesh( new THREE.SphereGeometry( 0.53, 32,16 ), matEye );
@@ -141,21 +113,7 @@ function onload(){
     head.animations[1].play(0);
     head.animations[2].stop(0);
 
-    /*suit.animations[0].stop(0);
-    suit.animations[1].play(0);
-    suit.animations[2].stop(0);*/
-
-    //upBone()
-
     v.scene.add(body);
     v.scene.add(suit);
 	v.scene.add(head);
-}
-
-function upBone(){
-    for(var key in headBones){
-        var mtx = bodyBones[key].matrix.clone();
-        headBones[key].matrix.copy(mtx)
-        headBones[key].matrixAutoUpdate = false;
-    }
 }

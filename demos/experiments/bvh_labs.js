75b303c8dd430dec1e4ce9dc57a8e25c8d69fd7e
lo-th <3dflashlo@gmail.com>
2015-04-07 15:01:09 +0200

    up

--- demos/experiments/bvh_labs.js
+++ demos/experiments/bvh_labs.js
@@ -8,6 +8,8 @@ bvh.load('images/bvh/action.png');
 
 v.pool.load('stickman', onload, true);
 
+
+
 loop();
 
 function loop(){
@@ -23,3 +25,21 @@ function onload(){
 	m.material = new THREE.MeshBasicMaterial({color:0xCCCCCC, skinning:true, envMap:v.environment, reflectivity:0.8, transparent:true, opacity:0.9})
 	bvh.skin(m);
 }
+
+
+var zone = document.createElement('div');
+zone.className = 'bvhDragZone';
+zone.innerHTML = 'Drag File';
+document.body.appendChild( zone );
+zone.ondragover = function () { this.className = 'bvhDragZone hover'; return false; };
+zone.ondragend = function () { this.className = 'bvhDragZone'; return false; };
+zone.ondrop = function (e) {
+	this.className = 'bvhDragZone';
+	e.preventDefault();
+	var file = e.dataTransfer.files[0];
+	var reader = new FileReader();
+	zone.innerHTML = file.name;
+    reader.onload = (function(theFile) { return function(e) { bvh.reader.parseData(e.target.result.split(/\s+/g));}; })(file);
+    reader.readAsText(file);
+    return false;
+};
